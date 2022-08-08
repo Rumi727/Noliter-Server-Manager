6d17b511cd5f74cae9c1ab4e306db910306be9e5
SimsimhanChobo <jun32820@gmail.com>
2022-08-09 01:10:09 +0900

    노트블록과 커맨드의 딜레이 차이를 없애기 위해 노트블록에 1틱 딜레이 추가

--- src/main/java/com/teambucket/kurumi/noliterservermanager/Convenience.java
+++ src/main/java/com/teambucket/kurumi/noliterservermanager/Convenience.java
@@ -3,11 +3,10 @@ package com.teambucket.kurumi.noliterservermanager;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import net.kyori.adventure.text.Component;
-import org.bukkit.GameMode;
-import org.bukkit.Instrument;
-import org.bukkit.Location;
-import org.bukkit.Note;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -60,6 +59,50 @@ public class Convenience implements Listener
             event.setCancelled(true);
         }
     }
+
+    @EventHandler
+    public void NotePlayEvent(NotePlayEvent notePlayEvent)
+    {
+        if (notePlayEvent.isCancelled())
+            return;
+
+        Block block = notePlayEvent.getBlock();
+        Location location = block.getLocation();
+        Instrument instrument = notePlayEvent.getInstrument();
+        Note note = notePlayEvent.getNote();
+        World world = block.getWorld();
+        boolean noDelay = false;
+
+        BlockState signBlock = world.getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ()).getState();
+        if (signBlock instanceof Sign)
+            noDelay = ((Sign)signBlock).line(0).equals(Component.text("no delay"));
+
+        signBlock = world.getBlockAt(location.getBlockX(), location.getBlockY() - 2, location.getBlockZ()).getState();
+        if (signBlock instanceof Sign && !noDelay)
+            noDelay = ((Sign)signBlock).line(0).equals(Component.text("no delay"));
+
+        if (!noDelay)
+        {
+            notePlayEvent.setCancelled(true);
+
+            new BukkitRunnable()
+            {
+                @Override
+                public void run()
+                {
+                    @SuppressWarnings("unchecked")
+                    Collection<Player> players = (Collection<Player>) Main.server.getOnlinePlayers();
+                    for (Player player : players)
+                    {
+                        //noinspection deprecation
+                        player.spawnParticle(Particle.NOTE,location.getX() + 0.5, location.getY() + 1, location.getZ() + 0.5, 0, note.getId() / 24d, 0, 0, 1);
+                        player.playNote(location, instrument, note);
+                    }
+                }
+            }.runTaskLater(Main.plugin, 1);
+        }
+    }
+
     public static void ActionBarCommand(Player player, Block block)
     {
         if (player.getGameMode() == GameMode.CREATIVE)
