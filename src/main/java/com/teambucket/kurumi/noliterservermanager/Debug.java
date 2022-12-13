5c22a82e487becdf61119dba5dc779838c909492
Simsimhan Chobo <jun32820@gmail.com>
2022-12-13 23:04:04 +0900

    Update Debug.java

--- src/main/java/com/teambucket/kurumi/noliterservermanager/Debug.java
+++ src/main/java/com/teambucket/kurumi/noliterservermanager/Debug.java
@@ -4,8 +4,5 @@ public class Debug
 {
     public static void Log(String message) { Main.debug.info("[NoliterManager] " + message); }
 
-    public static void LogWarning(String message)
-    {
-        Main.debug.warning("[NoliterManager] " + message);
-    }
+    public static void LogNoTitle(String message) { Main.debug.info(message); }
 }
