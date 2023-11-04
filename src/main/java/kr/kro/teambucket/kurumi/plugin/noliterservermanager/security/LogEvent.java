de970cc16faf6369ce8f324328c88469503ad3a0
Simsimhan Chobo <jun32820@gmail.com>
2023-11-04 18:47:30 +0900

    플레이어가 죽었을때 어디 위치에서 죽었는지 서버 콘솔에 기록하는 이벤트 추가

--- src/main/java/kr/kro/teambucket/kurumi/plugin/noliterservermanager/security/LogEvent.java
+++ src/main/java/kr/kro/teambucket/kurumi/plugin/noliterservermanager/security/LogEvent.java
@@ -5,12 +5,25 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class LogEvent implements Listener
 {
     @EventHandler
     public void OnPlayerQuit(PlayerQuitEvent event)
+    {
+        Player player = event.getPlayer();
+        Location position = player.getLocation();
+        double x = position.getX();
+        double y = position.getY();
+        double z = position.getZ();
+
+        Debug.LogNoTitle("[" + player.getWorld().getName() + "] " + x + ", " + y + ", " + z);
+    }
+
+    @EventHandler
+    public void OnPlayerDeath(PlayerDeathEvent event)
     {
         Player player = event.getPlayer();
         Location position = player.getLocation();
